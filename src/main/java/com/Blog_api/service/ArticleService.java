6b0e56fa90fc6429b685d147597da15d836019b8
Jesufemi-A <jesufemia1@gmail.com>
2024-06-20 19:29:35 -0700

    added articleServices for CRUD operations in ArticleController

--- src/main/java/com/Blog_api/service/ArticleService.java
+++ src/main/java/com/Blog_api/service/ArticleService.java
@@ -1,2 +1,44 @@
-package com.Blog_api.service;public class ArticleService {
+package com.Blog_api.service;
+
+
+import com.Blog_api.model.Article;
+import com.Blog_api.repository.ArticleRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+import java.util.*;
+import java.util.Optional;
+
+@Service
+@Component
+public class ArticleService {
+
+
+    private final ArticleRepository articleRepository;
+
+    public ArticleService(ArticleRepository articleRepository) {
+        this.articleRepository = articleRepository;
+    }
+
+    public void addArticle(Article article) {
+        articleRepository.save(article);
+    }
+
+    public List<Article> getArticles() {
+        return articleRepository.findAll();
+    }
+
+    public Article getArticlesById(Integer i) {
+        Optional<Article> optionalArticle = articleRepository.findById(i);
+
+        if (optionalArticle.isPresent()) {
+            Article article = optionalArticle.get();
+            return article;
+        } else {
+            return null;
+        }
+    }
+
+
+
+
 }
